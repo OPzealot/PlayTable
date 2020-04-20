b0cb0d508f7e99ffee3e585e8e8b977871b4e83a
liusili <liusili@unionbigdata.com>
2020-04-20 09:48:38 +0800

    Add modify xml features.

--- PlayTable.py
+++ PlayTable.py
@@ -12,6 +12,7 @@ import pandas as pd
 import os
 import shutil
 from tqdm import tqdm
+import xml.etree.ElementTree as ET
 
 
 class PlayTable(object):
@@ -21,6 +22,23 @@ class PlayTable(object):
         :param table_path:
         """
         self.sheet = pd.read_excel(table_path, index_col=index_col, sheet_name=sheet)
+
+    @staticmethod
+    def get_and_check(root, name, length):
+        """
+        :param root: Element-tree 根节点
+        :param name: 需要返回的子节点名称
+        :param length: 确认子节点长度
+        """
+        var_lst = root.findall(name)
+        if len(var_lst) == 0:
+            raise NotImplementedError('Can not find %s in %s.' % (name, root.tag))
+        if (length > 0) and (len(var_lst) != length):
+            raise NotImplementedError('The size of %s is supposed to be %d, but is %d.'
+                                      % (name, length, len(var_lst)))
+        if length == 1:
+            var_lst = var_lst[0]
+        return var_lst
 
     def move_judge(self, sample_root, dir_name='judge_correct'):
         """
@@ -55,10 +73,20 @@ class PlayTable(object):
             if os.path.isfile(xml_path):
                 new_xml_path = os.path.join(new_cat_path, xml_name)
                 shutil.copyfile(xml_path, new_xml_path)
+                
+                # 更改xml标签信息
+                tree = ET.parse(new_xml_path)
+                root = tree.getroot()
+                for obj in root.findall('object'):
+                    name = self.get_and_check(obj, 'name', 1).text
+                    if name == old_cat:
+                        self.get_and_check(obj, 'name', 1).text = judge_cat
+                tree.write(new_xml_path)
 
             pbar.set_description('Processing category {}'.format(old_cat))
 
         print('---End cleaning data---')
+
 
 if __name__ == '__main__':
     table_path = r'D:\Working\Tianma\13902\file\复判数据\13902_0417复判结果.xlsx'
