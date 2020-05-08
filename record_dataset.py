ec3a09eb7849475f4dc48f3d45296d78e722bdfa
liusili <liusili@unionbigdata.com>
2020-05-08 11:39:21 +0800

    add functions.

---
+++ record_dataset.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# encoding:utf-8
+"""
+author: liusili
+@contact: liusili@unionbigdata.com
+@software:
+@file: record_dataset
+@time: 2020/5/8
+@desc: 
+"""
+import xml.etree.ElementTree as ET
+import os
+import pandas as pd
+from tqdm import tqdm
+
+
+def get_and_check(root, name, length):
+    """
+    :param root: Element-tree 根节点
+    :param name: 需要返回的子节点名称
+    :param length: 确认子节点长度
+    """
+    var_lst = root.findall(name)
+    if len(var_lst) == 0:
+        raise NotImplementedError('Can not find %s in %s.' % (name, root.tag))
+    if (length > 0) and (len(var_lst) != length):
+        raise NotImplementedError('The size of %s is supposed to be %d, but is %d.'
+                                  % (name, length, len(var_lst)))
+    if length == 1:
+        var_lst = var_lst[0]
+    return var_lst
+
+
+def get_info_from_xml(xml_path, mode='max_area'):
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+    area = 0
+
+    for obj in root.findall('object'):
+        bbox = get_and_check(obj, 'bndbox', 1)
+        xmin = int(get_and_check(bbox, 'xmin', 1).text)
+        ymin = int(get_and_check(bbox, 'ymin', 1).text)
+        xmax = int(get_and_check(bbox, 'xmax', 1).text)
+        ymax = int(get_and_check(bbox, 'ymax', 1).text)
+        bbox_area = (xmax - xmin + 1) * (ymax - ymin + 1)
+        if mode == 'max_area':
+            if bbox_area > area:
+                area = bbox_area
+                category = get_and_check(obj, 'name', 1).text
+                bbox = [xmin, ymin, xmax, ymax]
+
+    return category, bbox
+
+
+def record_dataset(sample_root, img_format='.jpg'):
+    columns = ['file_name', 'category', 'bbox']
+    df = pd.DataFrame(columns=columns)
+    id = 0
+    pbar = tqdm(os.walk(sample_root))
+    for root, _, file_lst in pbar:
+        if len(file_lst) > 0:
+            for file in file_lst:
+                if os.path.splitext(file)[-1] == img_format:
+                    img_file = file
+                    file_name = os.path.splitext(file)[0]
+                    xml_path = os.path.join(root, file_name + '.xml')
+                    if os.path.isfile(xml_path):
+                        category, bbox = get_info_from_xml(xml_path)
+                        id += 1
+                        df.loc[id] = [img_file, category, bbox]
+        pbar.set_description('Processing category [{}]:'.format(category))
+
+    record_path = sample_root + '_record.xlsx'
+    df.to_excel(record_path, sheet_name='data')
+    print('[FINISH] Dataset has been recorded.')
+
+
+if __name__ == '__main__':
+    sample_root = r'D:\Working\Tianma\13902\data\13902_0429'
+    record_dataset(sample_root)
